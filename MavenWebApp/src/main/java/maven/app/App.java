0f52389b7d27ab74f70bb48daec2976c8493deae
VipulKumarSharma <tysonvks@gmail.com>
2017-10-08 18:07:48 +0530

    Commiting MavenWebApp
    Created from command line.

---
+++ MavenWebApp/src/main/java/maven/app/App.java
@@ -0,0 +1,11 @@
+package maven.app;
+
+import java.util.*;
+
+public class App {
+    
+	public static void main( String[] args ) {
+		List<String> list = new ArrayList<String>();
+        System.out.println( "List"+list );
+    }
+}
